a7624b048498db703c15b40b84e7350651897445
Mrityunjay Pathak <61449824+pathakmrityunjaykumar@users.noreply.github.com>
2022-12-30 14:09:56 +0530

    Add files via upload

---
+++ array/MinMax.java
@@ -0,0 +1,23 @@
+package array;
+import java.util.*;
+
+public class MinMax {
+public static void main(String[] args){
+    Scanner sc = new Scanner(Sstem.in);
+
+    int size = sc.nextInt();
+    int number[] = new int[size];
+
+        for(int i=0;i<size;i++){
+            number[i]=sc.nextInt();
+        }
+
+        System.out.println("the output  of max and min value:-");
+
+        
+    
+    
+    }
+    
+
+    }
